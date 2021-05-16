8561598700f96c60b822ac72e00c4d40bebe12b4
RedJake <cjakczerwonka@gmail.com>
2021-05-16 11:25:04 +0200

    fixes

--- src/main/java/red/jake/mgr/pojo/JoinExperiment.java
+++ src/main/java/red/jake/mgr/pojo/JoinExperiment.java
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package red.jake.mgr.pojo;
 
 import org.apache.flink.api.java.DataSet;
@@ -43,11 +25,9 @@ public class JoinExperiment extends BaseJob {
     public void runJob() throws Exception {
         DataSet<RowAirline> airlines = SourceFactory.getAirlineTypedSource(env, EnvironmentType.valueOf(envType));
         DataSet<RowCarrier> carriers = SourceFactory.getCarrierTypedSource(env);
-
         JoinOperator.DefaultJoin<RowAirline, RowCarrier> joined = airlines.joinWithTiny(carriers)
                 .where(AirlineHeader.uniqueCarrier)
                 .equalTo(CarrierHeader.code);
-
         joined.print();
     }
 }
